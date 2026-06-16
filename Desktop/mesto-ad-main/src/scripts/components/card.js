368cc39d1799a61d778724cb2ae1b96ee269645c
saiddavletmurzaev91-boop <saiddavletmurzaev91@gmail.com>
2026-06-16 11:52:10 +0300

    Update card.js

--- Desktop/mesto-ad-main/src/scripts/components/card.js
+++ Desktop/mesto-ad-main/src/scripts/components/card.js
@@ -1,56 +1,78 @@
-export const likeCard = (updatedCard, likeButton, likeCounter) => {
+```js
+const cardTemplateSelector = "#card-template";
+
+const cloneCardTemplate = () => {
+  const template = document.querySelector(cardTemplateSelector).content;
+  return template.querySelector(".card").cloneNode(true);
+};
+
+const hasUserLike = (likes, userId) => {
+  return likes.some((user) => user._id === userId);
+};
+
+export const isCardLiked = hasUserLike;
+
+export const likeCard = (cardData, likeButton, counterElement) => {
   likeButton.classList.toggle("card__like-button_is-active");
-  likeCounter.textContent = updatedCard.likes.length;
+  counterElement.textContent = cardData.likes.length;
 };
 
-
-
-const getTemplate = () => {
-  return document
-    .getElementById("card-template")
-    .content.querySelector(".card")
-    .cloneNode(true);
-};
-
-export const isCardLiked = (likesArray, userId) => {
-  return likesArray.some((like) => like._id === userId);
-};
-
-export const createCardElement = (
-  data,
-  { currentUserId, onPreviewPicture, onLikeClick, onDeleteClick }
-) => {
-  const cardElement = getTemplate();
-
-  const cardImage = cardElement.querySelector(".card__image");
-  const likeButton = cardElement.querySelector(".card__like-button");
-  const likeCounter = cardElement.querySelector(".card__like-count");
-  const deleteButton = cardElement.querySelector(".card__control-button_type_delete");
-  const cardTitle = cardElement.querySelector(".card__title");
-  cardImage.src = data.link;
-  cardImage.alt = data.name;
-  cardTitle.textContent = data.name;
-  
-  cardImage.addEventListener("click", () =>
-    onPreviewPicture({ name: data.name, link: data.link })
-  );
-  likeCounter.textContent = data.likes.length;
-  if (data.likes.some((l) => l._id === currentUserId)) {
-    likeButton.classList.add("card__like-button_is-active");
-  }
-  likeButton.addEventListener("click", () => {
-  const isLiked = likeButton.classList.contains("card__like-button_is-active");
-  onLikeClick(likeButton, data._id, likeCounter, isLiked);
-});
-  if (data.owner._id !== currentUserId) {
-    deleteButton.remove();
-  } else {
-    deleteButton.addEventListener("click", () =>
-      onDeleteClick(cardElement, data._id)
-    );
-  }
-  return cardElement;
-};
 export const removeCard = (cardElement) => {
   cardElement.remove();
 };
+
+export const createCardElement = (
+  cardData,
+  { currentUserId, onPreviewPicture, onLikeClick, onDeleteClick }
+) => {
+  const card = cloneCardTemplate();
+
+  const imageElement = card.querySelector(".card__image");
+  const titleElement = card.querySelector(".card__title");
+  const likeButton = card.querySelector(".card__like-button");
+  const likesCounter = card.querySelector(".card__like-count");
+  const deleteButton = card.querySelector(
+    ".card__control-button_type_delete"
+  );
+
+  imageElement.src = cardData.link;
+  imageElement.alt = cardData.name;
+  titleElement.textContent = cardData.name;
+
+  likesCounter.textContent = cardData.likes.length;
+
+  if (hasUserLike(cardData.likes, currentUserId)) {
+    likeButton.classList.add("card__like-button_is-active");
+  }
+
+  imageElement.addEventListener("click", () => {
+    onPreviewPicture({
+      name: cardData.name,
+      link: cardData.link,
+    });
+  });
+
+  likeButton.addEventListener("click", () => {
+    const likedByCurrentUser = likeButton.classList.contains(
+      "card__like-button_is-active"
+    );
+
+    onLikeClick(
+      likeButton,
+      cardData._id,
+      likesCounter,
+      likedByCurrentUser
+    );
+  });
+
+  if (cardData.owner._id === currentUserId) {
+    deleteButton.addEventListener("click", () => {
+      onDeleteClick(card, cardData._id);
+    });
+  } else {
+    deleteButton.remove();
+  }
+
+  return card;
+};
+```
